82e4985eba0c182630a2ea8de8a406c28a50db2d
George Kauan <130989680+kzinge@users.noreply.github.com>
2025-05-05 15:18:07 -0300

    atualizado

--- aula01/main.py
+++ aula01/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, HTTPException
 from models import Tarefa
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
 app = FastAPI()
 
 #Salva tarefas na memória, sem banco
 tarefas: List[Tarefa] = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 #Listar
 @app.get("/tarefas/", response_model=list[Tarefa])
